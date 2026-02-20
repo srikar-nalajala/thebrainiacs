cc2ad64983528d7ff2d81f81077f570fb82bce24
SrikarNalajala <257r1a05h4@cmrtc.ac.in>
2026-02-20 21:38:54 +0530

    Fix localhost 404 by making basePath dynamic for production

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
-    basePath: '/thebrainiacs',
-    assetPrefix: '/thebrainiacs/',
+    basePath: process.env.NODE_ENV === 'production' ? '/thebrainiacs' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/thebrainiacs/' : '',
 };
 
 module.exports = nextConfig;
