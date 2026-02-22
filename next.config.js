61d3163a5c18d1ca8e4e4b19e041027b9e783c08
SrikarNalajala <257r1a05h4@cmrtc.ac.in>
2026-02-22 21:38:22 +0530

    Fix GitHub Pages 404: Set trailingSlash true and add .nojekyll

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
+    trailingSlash: true,
     basePath: process.env.NODE_ENV === 'production' ? '/thebrainiacs' : '',
     assetPrefix: process.env.NODE_ENV === 'production' ? '/thebrainiacs/' : '',
 };
